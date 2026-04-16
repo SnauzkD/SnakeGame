59d9d1426036b9125b3df7506ab80e8145466bea
SnauzkD <126462005+SnauzkD@users.noreply.github.com>
2026-04-16 17:12:12 +0500

    Initial commit

---
+++ SnakeGame.cpp
@@ -0,0 +1,12 @@
+﻿// SnakeGame.cpp : Defines the entry point for the application.
+//
+
+#include "SnakeGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
